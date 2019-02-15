95eb1047212ba2e915e1a945434788c8d7a50950
timueh <tillmann.muehlpfordt@kit.edu>
2019-02-15 11:11:37 +0100

    improved solution for hamming using `map`

--- julia/hamming/hamming.jl
+++ julia/hamming/hamming.jl
@@ -1,5 +1,4 @@
 function distance(s1::String, s2::String)::Int
     length(s1) != length(s2) ? throw(ArgumentError("strings do not have same length")) : nothing
-    cmp = ( collect(s1) .== collect(s2) )
-    count(map(!,cmp)) # length(s1) - count(cmp)
+    count(map(!=,s1,s2))
 end
