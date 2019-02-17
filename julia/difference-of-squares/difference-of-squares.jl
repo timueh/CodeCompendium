948f852ceb38fe56867498a8f9a820bfcc80d86e
timueh <tillmann.muehlpfordt@kit.edu>
2019-02-17 12:29:05 +0100

    final version for difference of squares

--- julia/difference-of-squares/difference-of-squares.jl
+++ julia/difference-of-squares/difference-of-squares.jl
@@ -1,16 +1,14 @@
 "Sum the squares of the numbers up to the given number"
-function square_of_sum(n::Int)::Int
-    Int(0.25 * n^2 * (n + 1)^2)
-end
+square_of_sum(n::Int) = (n^2 * (n + 1)^2) ÷ 4
 
 "Square the sum of the numbers up to the given number"
-function sum_of_squares(n::Int)::Int
-    Int((n * (n + 1) * (2 * n + 1)) / 6);
-end
+sum_of_squares(n::Int) = (n * (n + 1) * (2 * n + 1)) ÷ 6
 
 "Subtract sum of squares from square of sums"
 function difference(n::Int)::Int
-    n < 0 ? throw(DomainError("`n has to be non-negative`")) : nothing
+    if n < 0 throw(DomainError("`n has to be non-negative`")) end
+    # what do you think of short-circuiting?
+    # n < 0 && throw(DomainError("`n has to be non-negative`"))
     square_of_sum(n) - sum_of_squares(n)
 end
 
