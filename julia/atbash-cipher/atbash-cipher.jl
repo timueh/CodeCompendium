210de173b997da61ef928b81a7cb18d2c88fc04e
timueh <tillmann.muehlpfordt@kit.edu>
2019-02-18 15:46:05 +0100

    inspired by leonistor's solution

--- julia/atbash-cipher/atbash-cipher.jl
+++ julia/atbash-cipher/atbash-cipher.jl
@@ -1,27 +1,16 @@
 function encode(input::String;N::Int=5)
-    s = decode(input)
-    if length(s)<=N
-        return s
-    else
-        # break into groups of size $N$ with trailing space
-        # chunks = collect(m.match for m in eachmatch(Regex("(.{1,$N})"),s))
-        ls = length(s)
-        chunkrange = 1:N:ls
-        chunks = Vector{String}()
-        sizehint!(chunks, length(chunkrange))
-        for r in chunkrange
-            endr = min(r+N, ls+1) - 1
-            push!(chunks, s[r:endr])
-        end
-        return join(chunks, " ")
-    end
+    clean = filter(c->isletter(c) || isnumeric(c), lowercase(input))
+    chunks = Iterators.partition(map(encode,clean),N)
+    join([join(chunk) for chunk in chunks]," ")
 end
 
-# core function -- needs to be called for lowercase `c`
+# core function -- needs to be called for lowercase `c` and clean input
 function encode(c::Char)
-    isnumeric(c)    && return string(c)
-    c in 'a' : 'z'  && return string(Char( 'z' - (c - 'a') ))
-    return ""
+    isnumeric(c) && return c
+    'z' - (c - 'a')
 end
 
-decode(input::String) = join(encode(letter) for letter in lowercase(input))
+function decode(input::String)
+    clean = filter(c->!isspace(c),input)
+    map(encode,clean)
+end
