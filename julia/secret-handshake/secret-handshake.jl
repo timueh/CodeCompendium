1c4a8b3c1f26fae89d38d09358e47472cb4f3bb1
timueh <tillmann.muehlpfordt@kit.edu>
2019-02-18 22:14:01 +0100

    convert to binary by hand

--- julia/secret-handshake/secret-handshake.jl
+++ julia/secret-handshake/secret-handshake.jl
@@ -4,12 +4,23 @@ function secret_handshake(code::Integer)
                     2=>"double blink",
                     3=>"close your eyes",
                     4=>"jump" )
-    s = reverse(string(code, base=2))
+    # @show s = parse(UInt16,bitstring(code)[leading_zeros(code)+1:end]) |> digits
+    s = int2bin(code)
     ends, rev = length(s) <= 4 ? (length(s), false) : (4, true)
     handshake = Vector{String}()
     # sizehint!(handshake,count_ones(code))
     for (i,letter) in enumerate(s[1:ends])
-        letter == '1' && push!(handshake,actions[i])
+        letter == 1 && push!(handshake,actions[i])
     end
     rev ? reverse(handshake) : handshake
+end
+
+function int2bin(n::Integer)
+    rems = Vector{UInt8}()
+    # sizehint!(rems,ceil(log2(n)))
+    while n > 0
+        push!(rems,n % 2)
+        n ÷= 2
+    end
+    rems
 end
