c321703abf3f1be3c29ddc05c4b90c54021d6560
timueh <tillmann.muehlpfordt@kit.edu>
2019-02-21 20:45:36 +0100

    approved secret handshake

--- julia/secret-handshake/secret-handshake.jl
+++ julia/secret-handshake/secret-handshake.jl
@@ -1,26 +1,11 @@
 function secret_handshake(code::Integer)
     (code <= 0 || code >= 32) && return []
-    actions = Dict( 1=>"wink",
-                    2=>"double blink",
-                    3=>"close your eyes",
-                    4=>"jump" )
-    # @show s = parse(UInt16,bitstring(code)[leading_zeros(code)+1:end]) |> digits
-    s = int2bin(code)
-    ends, rev = length(s) <= 4 ? (length(s), false) : (4, true)
+    actions = ["wink", "double blink", "close your eyes", "jump"]
+    bits = 0:4
+    s = map(i->code & 2^i == 2^i,bits)
     handshake = Vector{String}()
-    # sizehint!(handshake,count_ones(code))
-    for (i,letter) in enumerate(s[1:ends])
-        letter == 1 && push!(handshake,actions[i])
+    for (i,l) in enumerate(s[1:end-1])
+        l && push!(handshake,actions[i])
     end
-    rev ? reverse(handshake) : handshake
-end
-
-function int2bin(n::Integer)
-    rems = Vector{UInt8}()
-    # sizehint!(rems,ceil(log2(n)))
-    while n > 0
-        push!(rems,n % 2)
-        n ÷= 2
-    end
-    rems
+    s[end] ? reverse(handshake) : handshake
 end
