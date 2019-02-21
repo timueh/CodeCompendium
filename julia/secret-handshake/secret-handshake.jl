3e5a5846b8936fb66126e8f6510978bac110b84a
timueh <tillmann.muehlpfordt@kit.edu>
2019-02-21 20:46:17 +0100

    improved secret handshake

--- julia/secret-handshake/secret-handshake.jl
+++ julia/secret-handshake/secret-handshake.jl
@@ -1,11 +1,7 @@
 function secret_handshake(code::Integer)
     (code <= 0 || code >= 32) && return []
     actions = ["wink", "double blink", "close your eyes", "jump"]
-    bits = 0:4
-    s = map(i->code & 2^i == 2^i,bits)
-    handshake = Vector{String}()
-    for (i,l) in enumerate(s[1:end-1])
-        l && push!(handshake,actions[i])
-    end
-    s[end] ? reverse(handshake) : handshake
+    pick = Bool.(digits(code, base=2, pad=5))
+    s = actions[pick[1:4]]
+    pick[end] ? reverse(s) : s
 end
