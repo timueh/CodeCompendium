9a0046b5c7dfa93ef0985e4c3e5f8f58c38ba1c8
timueh <tillmann.muehlpfordt@kit.edu>
2019-05-28 22:00:42 +0200

    improved raindrops in python

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,10 +1,4 @@
 def raindrops(number):
     d = { 3:"Pling", 5:"Plang", 7:"Plong" }
-    s = ""
-    for digit, s_ in d.items():
-        if number % digit == 0: s += s_
-    # alternative one-liner:
-    # s = "".join( map(lambda x: d[x] if number % x == 0 else "", d.keys()) )
-    if not s:
-        return str(number)
-    return s
+    s = [ word for (digit, word) in d.items() if number % digit == 0 ]
+    return "".join(s) or str(number)
