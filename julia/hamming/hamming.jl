d849dde209f6df3a4c011e5da0865898174bc096
timueh <tillmann.muehlpfordt@kit.edu>
2019-06-10 20:03:12 +0200

    mini fix in hamming.jl

--- julia/hamming/hamming.jl
+++ julia/hamming/hamming.jl
@@ -1,4 +1,4 @@
 function distance(s1::String, s2::String)::Int
-    length(s1) != length(s2) ? throw(ArgumentError("strings do not have same length")) : nothing
+    length(s1) != length(s2) && throw(ArgumentError("strings do not have same length"))
     count(map(!=,s1,s2))
 end
