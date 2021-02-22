6eb5feeaea856b54a1c3f226f6567ac04f26c9b0
timueh <tillmann.muehlpfordt@kit.edu>
2021-02-22 21:27:01 +0100

    bash two-fer added

---
+++ bash/two-fer/two_fer.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# This is a bash script template in order to help you quick start any script.
+# It contains some sensible defaults, you can learn more by visiting:
+# https://google.github.io/styleguide/shell.xml
+#
+# /!\ Please remove all the comments when you use this on other exercises
+
+# This option will make the script exit when there is an error
+set -o errexit
+# This option will make the script exit when it tries to use an unset variable
+set -o nounset
+
+main() {
+  name=${1:-you}
+  echo "One for $name, one for me."
+}
+
+# Calls the main function passing all the arguments to it via '$@'
+# The argument is in quotes to prevent whitespace issues
+main "$@"
