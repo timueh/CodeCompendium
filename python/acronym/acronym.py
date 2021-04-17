a7e6dd9d533a6224f6fdadd5722435f3ca6397e1
timueh <tillmann.muehlpfordt@kit.edu>
2021-04-17 21:14:22 +0200

    updated acronym.py

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -2,26 +2,12 @@ import string
 
 
 def pre_process(words):
-    for char in ["-", "_"]:
-        words = words.replace(char, " ")
+    for char in ['-', '_']:
+        words = words.replace(char, ' ')
     return words
-
-
-def get_initial(word):
-    if len(word) == 0:
-        return ""
-    else:
-        for char in word:
-            if char.isalpha():
-                return char.upper()
 
 
 def abbreviate(words):
     words = pre_process(words)
-    split_words = words.split(" ")
-    first_letters = map(get_initial, split_words)
-    return "".join(first_letters)
-
-
-if __name__ == "__main__":
-    print(abbreviate("Complementary metal-oxide semiconductor"))
+    first_letters = map(lambda x: x[0].upper() if len(x) > 0 else '', words.split(' '))
+    return ''.join(first_letters)
